41c633ed1d0e8dcfc5ed554670132ec049fbb709
Paul-maire <paul.maire@hetic.net>
2021-05-17 23:46:03 +0200

    knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,13 @@
 module.exports = {
   client: 'pg',
-  connection: process.env.DATABASE_URL,
+  connection: { 
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+  },
   migrations: {
     directory: `${__dirname}/db/migrations`
   },
   seeds: {
     directory: `${__dirname}/db/seeds`
   },
-  ssl: true  
 }
